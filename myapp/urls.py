10b6d1c1405772d8e75c8b1fdc6f5063e3e58b39
jignesh <jigneshpatel.ec.er@gmail.com>
2021-08-11 16:44:51 +0530

    delete and edit done with alert

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,8 @@ from .import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('save/',views.save_data,name='save'),
-    
+    path('delete/',views.delete_data,name='delete'),
+    path('edit/',views.edit,name='edit'),
+    path('edit/<str:user_id>/',views.edit,name='edit'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.db import reset_queries
 from django.http.response import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import User
 
 from django.views.decorators.csrf import csrf_exempt
@@ -15,7 +15,7 @@ def index(request):
     return render(request,'index.html',params)
 
 @csrf_exempt 
-
+#insert data
 def save_data(request):
     if request.method=="POST":
         s=User()
@@ -29,4 +29,30 @@ def save_data(request):
     else:
        return JsonResponse({'status':0})
 
+#delete data
+@csrf_exempt 
+def delete_data(request):
+    if request.method=="POST":
+        id=request.POST.get('sid')
+        print(id)
+        pi=User.objects.get(pk=id)
+        pi.delete()
+        return JsonResponse({'status':1})
+    else:
+        return JsonResponse({'status':0})
 
+       
+
+@csrf_exempt 
+def edit(request,user_id = None):
+    if request.method=="POST":
+        s=User.objects.get(id=request.POST['user_id'])
+        s.name=request.POST['name']
+        s.email=request.POST['email']
+        s.password=request.POST['password']
+        s.save()
+        return redirect('/')
+    else:
+        s=User.objects.get(id=user_id)
+        params = {'user': s}
+        return render(request,'edit.html', params) 
